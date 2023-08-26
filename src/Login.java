23ad24b869f53e1a9892d2cfe33f4744b973218b
Adarsh11Sharma <adarshdsharma.11@gmail.com>
2023-08-26 21:57:15 +0530

    made rules and quiz(temolate)

--- src/Login.java
+++ src/Login.java
@@ -1,58 +1,56 @@
 
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Login extends JFrame implements ActionListener {
+public class Login extends JFrame implements ActionListener{
 
-    JButton rules, back; // Declare class-level buttons
+    JButton rules, back;
     JTextField tfname;
 
     Login() {
-        // Setting up frame properties
-        setSize(800, 400); // Adjusted size
-        setLocation(200, 150);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // Set the layout and background color
-        setLayout(null);
         getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
 
-        // Creating and adding an image label
-        ImageIcon i1 = new ImageIcon(getClass().getResource("/icons/login.jpeg"));
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/login.jpeg"));
         JLabel image = new JLabel(i1);
-        image.setBounds(0, 0, 300, 400); // Adjusted size
+        image.setBounds(0, 0, 600, 500);
         add(image);
 
-        // Creating and adding a heading label
-        JLabel Heading = new JLabel("Simple Minds");
-        Heading.setBounds(350, 20, 300, 45); // Adjusted position
-        add(Heading);
-        Heading.setFont(new Font("Viner Hand ITC", Font.BOLD, 25));
-        Heading.setForeground(Color.BLUE);
+        JLabel heading = new JLabel("Simple Minds");
+        heading.setBounds(750, 60, 300, 45);
+        heading.setFont(new Font("Viner Hand ITC", Font.BOLD, 40));
+        heading.setForeground(new Color(30, 144, 254));
+        add(heading);
+
+        JLabel name = new JLabel("Enter your name");
+        name.setBounds(810, 150, 300, 20);
+        name.setFont(new Font("Mongolian Baiti", Font.BOLD, 18));
+        name.setForeground(new Color(30, 144, 254));
+        add(name);
 
         tfname = new JTextField();
-        tfname.setBounds(350, 100, 300, 25); // Adjusted position
+        tfname.setBounds(735, 200, 300, 25);
         tfname.setFont(new Font("Times New Roman", Font.BOLD, 20));
         add(tfname);
 
-        // ... Other GUI components ...
-
-        rules = new JButton("Rules"); // Use the class-level variable
-        rules.setBounds(350, 160, 120, 30); // Adjusted position and size
+        rules = new JButton("Rules");
+        rules.setBounds(735, 270, 120, 25);
         rules.setBackground(new Color(30, 144, 254));
+        rules.setForeground(Color.WHITE);
         rules.addActionListener(this);
         add(rules);
 
-        back = new JButton("Back"); // Use the class-level variable
-        back.setBounds(530, 160, 120, 30); // Adjusted position and size
+        back = new JButton("Back");
+        back.setBounds(915, 270, 120, 25);
         back.setBackground(new Color(30, 144, 254));
+        back.setForeground(Color.WHITE);
         back.addActionListener(this);
         add(back);
 
-        // ... Other GUI components ...
-
-        // Making the frame visible
+        setSize(1200, 500);
+        setLocation(200, 150);
         setVisible(true);
     }
 
@@ -60,15 +58,14 @@ public class Login extends JFrame implements ActionListener {
         if (ae.getSource() == rules) {
             String name = tfname.getText();
             setVisible(false);
-            new Rules(name); // Assuming you have a Rules class for the new screen
+            new Rules(name);
         } else if (ae.getSource() == back) {
             setVisible(false);
-            // Handle back button action, e.g., going back to a previous screen
         }
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new Login());
+        new Login();
     }
 }
 
